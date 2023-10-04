2d709b040936941cb5a9cb89bb96c9e83d75a49f
Keinsleif <mountaindull@gmail.com>
2023-10-04 15:33:53 +0900

    [update] split LDA & ZeroNNegative function

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,25 @@ impl CPU {
         }
     }
 
+    fn lda(&mut self,value: u8) {
+        self.reg_a = value;
+        self.update_zero_n_negative_flag(self.reg_a)
+    }
+
+    fn update_zero_n_negative_flag(&mut self,result: u8) {
+        if result == 0 {
+            self.status = self.status | 0b0000_0010;
+        } else {
+            self.status = self.status & 0b1111_1101;
+        }
+
+        if result & 0b1000_0000 != 0 {
+            self.status = self.status | 0b1000_0000;
+        } else {
+            self.status = self.status & 0b0111_1111;
+        }
+    }
+
     pub fn interpret(&mut self, program: Vec<u8>) {
         self.pc = 0;
         
@@ -24,19 +43,7 @@ impl CPU {
                 0xA9 => {
                     let param = program[self.pc as usize];
                     self.pc += 1;
-                    self.reg_a = param;
-
-                    if self.reg_a == 0 {
-                        self.status = self.status | 0b0000_0010;
-                    } else {
-                        self.status = self.status & 0b1111_1101;
-                    }
-
-                    if self.reg_a & 0b1000_0000 != 0 {
-                        self.status = self.status | 0b1000_0000;
-                    } else {
-                        self.status = self.status & 0b0111_1111;
-                    }
+                    self.lda(param)
                 }
                 0x00 => {
                     return;
