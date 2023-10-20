2ed763f3854d775bffbc695857f8c57ff3ffc956
Keinsleif <mountaindull@gmail.com>
2023-10-20 15:06:21 +0900

    [add] callback in run func

--- src/cpu.rs
+++ src/cpu.rs
@@ -103,7 +103,16 @@ impl CPU {
     }
 
     pub fn run(&mut self) {
+        self.run_with_callback(|_| {});
+    }
+
+    pub fn run_with_callback<F>(&mut self, mut callback: F)
+    where
+        F: FnMut(&mut CPU),
+    {
         loop {
+            callback(self);
+
             let code = self.mem_read(self.program_counter);
             self.program_counter += 1;
             let pc_state = self.program_counter;
