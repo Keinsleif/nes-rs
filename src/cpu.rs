6390f11db92cf0653424e15e29d1c0432a7016b2
Keinsleif <mountaindull@gmail.com>
2023-10-29 02:35:44 +0900

    [update] Mem trait for cpu

--- src/cpu.rs
+++ src/cpu.rs
@@ -26,6 +26,34 @@ pub struct CPU {
     memory: [u8; 0xFFFF],
 }
 
+trait Mem {
+    fn mem_read(&self, addr: u16) -> u8;
+    fn mem_write(&mut self, addr: u16, data: u8);
+
+    fn mem_read_u16(&self, pos: u16) -> u16 {
+        let low = self.mem_read(pos) as u16;
+        let high = self.mem_read(pos + 1) as u16;
+        (high << 8) | low
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let high = (data >> 8) as u8;
+        let low = (data & 0xFF) as u8;
+        self.mem_write(pos, low);
+        self.mem_write(pos + 1, high)
+    }
+}
+
+impl Mem for CPU {
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+}
+
 impl CPU {
     pub fn new() -> Self {
         CPU {
@@ -60,27 +88,6 @@ impl CPU {
         let low = self.stack_pop() as u16;
         let high = self.stack_pop() as u16;
         high << 8 | low
-    }
-
-    pub fn mem_read(&self, addr: u16) -> u8 {
-        self.memory[addr as usize]
-    }
-
-    pub fn mem_write(&mut self, addr: u16, data: u8) {
-        self.memory[addr as usize] = data;
-    }
-
-    pub fn mem_read_u16(&self, pos: u16) -> u16 {
-        let low = self.mem_read(pos) as u16;
-        let high = self.mem_read(pos + 1) as u16;
-        (high << 8) | low
-    }
-
-    pub fn mem_write_u16(&mut self, pos: u16, data: u16) {
-        let high = (data >> 8) as u8;
-        let low = (data & 0xFF) as u8;
-        self.mem_write(pos, low);
-        self.mem_write(pos + 1, high)
     }
 
     pub fn load_and_run(&mut self, program: Vec<u8>) {
