3b4b8e067357f346d9792fdbf777261532655860
Keinsleif <mountaindull@gmail.com>
2023-11-03 18:13:34 +0900

    [update] skip screen init

--- src/main.rs
+++ src/main.rs
@@ -67,20 +67,20 @@ fn color(byte: u8) -> Color {
 
 fn main() {
     // init sdl2
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsys = sdl_context.video().unwrap();
-    let window = video_subsys
-    .window("Snake Game", (32.0 * 10.0) as u32 , (32.0 * 10.0) as u32)
-    .position_centered()
-    .build().unwrap();
+    // let sdl_context = sdl2::init().unwrap();
+    // let video_subsys = sdl_context.video().unwrap();
+    // let window = video_subsys
+    // .window("Snake Game", (32.0 * 10.0) as u32 , (32.0 * 10.0) as u32)
+    // .position_centered()
+    // .build().unwrap();
 
-    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    canvas.set_scale(10.0, 10.0).unwrap();
+    // let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    // let mut event_pump = sdl_context.event_pump().unwrap();
+    // canvas.set_scale(10.0, 10.0).unwrap();
 
-    // setup texture
-    let creator = canvas.texture_creator();
-    let mut texture = creator.create_texture_target(PixelFormatEnum::RGB24, 32, 32).unwrap();
+    // // setup texture
+    // let creator = canvas.texture_creator();
+    // let mut texture = creator.create_texture_target(PixelFormatEnum::RGB24, 32, 32).unwrap();
 
     let bytes = std::fs::read("nestest.nes").unwrap();
     let rom = Rom::new(&bytes).unwrap();
@@ -90,8 +90,8 @@ fn main() {
     cpu.reset();
     cpu.program_counter = 0xC000;
 
-    let mut screen_state = [0 as u8; 32 * 3 * 32];
-    let mut rng = rand::thread_rng();
+    // let mut screen_state = [0 as u8; 32 * 3 * 32];
+    // let mut rng = rand::thread_rng();
 
     cpu.run_with_callback(move |cpu| {
         println!("{}", trace(cpu));
