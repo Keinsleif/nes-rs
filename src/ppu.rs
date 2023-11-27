93c6fb2e9f29dab405be860c5b26041dca59d709
Keinsleif <mountaindull@gmail.com>
2023-11-27 18:21:46 +0900

    [update] add ctrl reg and mirroring read

--- src/ppu.rs
+++ src/ppu.rs
@@ -12,6 +12,9 @@ pub struct NesPPU {
 
     pub mirroring: Mirroring,
     addr: registers::AddrRegister,
+
+    pub ctrl: ControlRegister,
+    internal_data_buf: u8,
 }
 
 impl NesPPU {
@@ -23,9 +26,67 @@ impl NesPPU {
             oam_data: [0; 64 * 4],
             palette_table: [0; 32],
             addr: AddrRegister::new(),
+            ctrl: ControlRegister::new(),
+            internal_data_buf: 0,
         }
     }
+
     fn write_to_ppu_addr(&mut self, value: u8) {
         self.addr.update(value);
+    }
+
+    fn write_to_ctrl(&mut self, value: u8) {
+        self.ctrl.update(value);
+    }
+
+    fn increment_vram_addr(&mut self) {
+        self.addr.increment(self.ctrl.vram_addr_increment());
+    }
+
+    // Horizontal:
+    //   [ A ] [ a ]
+    //   [ B ] [ b ]
+ 
+    // Vertical:
+    //   [ A ] [ B ]
+    //   [ a ] [ b ]
+    pub fn mirror_vram_addr(&self, addr: u16) -> u16 {
+    let mirrored_vram = addr & 0b10111111111111; // mirror down 0x3000-0x3eff to 0x2000 - 0x2eff
+    let vram_index = mirrored_vram - 0x2000; // to vram vector
+    let name_table = vram_index / 0x400; // to the name table index
+    match (&self.mirroring, name_table) {
+        (Mirroring::VERTICAL, 2) | (Mirroring::VERTICAL, 3) => vram_index - 0x800,
+        (Mirroring::HORIZONTAL, 2) => vram_index - 0x400,
+        (Mirroring::HORIZONTAL, 1) => vram_index - 0x400,
+        (Mirroring::HORIZONTAL, 3) => vram_index - 0x800,
+        _ => vram_index,
+    }
+}
+ 
+ 
+    fn read_data(&mut self) -> u8 {
+        let addr = self.addr.get();
+        self.increment_vram_addr();
+ 
+ 
+ 
+        match addr {
+            0..=0x1fff => {
+                let result = self.internal_data_buf;
+                self.internal_data_buf = self.chr_rom[addr as usize];
+                result
+            },
+            0x2000..=0x2fff => {
+                let result = self.internal_data_buf;
+                self.internal_data_buf = self.vram[self.mirror_vram_addr(addr) as usize];
+                result
+            },
+            0x3000..=0x3eff => panic!("addr space 0x3000..0x3eff is not expected to be used, requested = {} ", addr),
+            0x3f00..=0x3fff =>
+            {
+                self.palette_table[(addr - 0x3f00) as usize]
+            }
+            _ => panic!("unexpected access to mirrored space {}", addr),
+        }
     }
 }
