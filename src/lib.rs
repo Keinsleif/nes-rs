338f181f89610632d19de985db4fe6c695604733
Keinsleif <mountaindull@gmail.com>
2023-11-30 17:21:07 +0900

    [update] mod interrupt.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod cpu;
 pub mod cartridge;
 pub mod trace;
 pub mod ppu;
+pub mod interrupt;
 
 #[macro_use]
 extern crate bitflags;
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod opcodes;
 pub mod cartridge;
 pub mod trace;
 pub mod ppu;
+pub mod interrupt;
 
 use bus::Bus;
 use cartridge::Rom;
