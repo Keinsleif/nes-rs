ff7eea03fbaab9b07efa954f693d6a4d9f564799
Keinsleif <mountaindull@gmail.com>
2023-11-30 13:15:59 +0900

    [add] ScrollRegister

--- src/ppu/registers.rs
+++ src/ppu/registers.rs
@@ -2,8 +2,10 @@ mod addr;
 mod control;
 mod mask;
 mod status;
+mod scroll;
 
 pub use addr::AddrRegister;
 pub use control::ControlRegister;
 pub use mask::MaskRegister;
 pub use status::StatusRegister;
+pub use scroll::ScrollRegister;
