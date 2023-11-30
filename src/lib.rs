0dab476761af3a1d9f25dcf0efa0ba5995a84aeb
Keinsleif <mountaindull@gmail.com>
2023-11-30 18:34:19 +0900

    [update] load renderer.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod cartridge;
 pub mod trace;
 pub mod ppu;
 pub mod interrupt;
+pub mod renderer;
 
 #[macro_use]
 extern crate bitflags;
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod cartridge;
 pub mod trace;
 pub mod ppu;
 pub mod interrupt;
+pub mod renderer;
 
 use bus::Bus;
 use cartridge::Rom;
