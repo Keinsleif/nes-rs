d2cfdd44c6ca4e5bfb9959dc9cedffe9f0b0e82d
Keinsleif <mountaindull@gmail.com>
2023-12-08 10:52:33 +0900

    [remove] unused import

--- src/main.rs
+++ src/main.rs
@@ -12,70 +12,15 @@ use std::collections::HashMap;
 
 use bus::Bus;
 use cartridge::Rom;
-use cpu::{CPU, Mem};
+use cpu::CPU;
 use joypad::Joypad;
 use ppu::NesPPU;
 use renderer::frame::Frame;
-use trace::trace;
-use rand::Rng;
-use sdl2::{pixels::{PixelFormatEnum, Color}, EventPump, event::Event, keyboard::Keycode};
+// use trace::trace;
+use sdl2::{pixels::PixelFormatEnum, event::Event, keyboard::Keycode};
 
 #[macro_use]
 extern crate bitflags;
-
-fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
-    for event in event_pump.poll_iter() {
-        match event {
-            Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                std::process::exit(0)
-            },
-            Event::KeyDown { keycode: Some(Keycode::W), .. } => {
-                cpu.mem_write(0xff, 0x77);
-            },
-            Event::KeyDown { keycode: Some(Keycode::S), .. } => {
-                cpu.mem_write(0xff, 0x73);
-            },
-            Event::KeyDown { keycode: Some(Keycode::A), .. } => {
-                cpu.mem_write(0xff, 0x61);
-            },
-            Event::KeyDown { keycode: Some(Keycode::D), .. } => {
-                cpu.mem_write(0xff, 0x64);
-            }
-            _ => {/* do nothing */}
-        }
-    }
-}
-
-fn color(byte: u8) -> Color {
-    match byte {
-        0 => sdl2::pixels::Color::BLACK,
-        1 => sdl2::pixels::Color::WHITE,
-        2 | 9 => sdl2::pixels::Color::GREY,
-        3 | 10 => sdl2::pixels::Color::RED,
-        4 | 11 => sdl2::pixels::Color::GREEN,
-        5 | 12 => sdl2::pixels::Color::BLUE,
-        6 | 13 => sdl2::pixels::Color::MAGENTA,
-        7 | 14 => sdl2::pixels::Color::YELLOW,
-        _ => sdl2::pixels::Color::CYAN,
-    }
- }
-
- fn read_screen_state(cpu: &mut CPU, frame: &mut [u8; 32 * 3 * 32]) -> bool {
-    let mut frame_idx = 0;
-    let mut update = false;
-    for i in 0x0200..0x600 {
-        let color_idx = cpu.mem_read(i as u16);
-        let (b1, b2, b3) = color(color_idx).rgb();
-        if frame[frame_idx] != b1 || frame[frame_idx + 1] != b2 || frame[frame_idx + 2] != b3 {
-            frame[frame_idx] = b1;
-            frame[frame_idx + 1] = b2;
-            frame[frame_idx + 2] = b3;
-            update = true;
-        }
-        frame_idx += 3;
-    }
-    update
- }
 
 fn main() {
     // init sdl2
