24849fdbd6b070f5cea99e5003d880b7bc05281c
Keinsleif <mountaindull@gmail.com>
2024-02-23 02:42:36 +0900

    [update] add brk interrupt

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,6 +1,7 @@
 #[derive(PartialEq, Eq)]
 pub enum InterruptType {
     NMI,
+    BRK,
 }
 
 #[derive(PartialEq, Eq)]
@@ -10,9 +11,17 @@ pub(super) struct Interrupt {
     pub(super) b_flag_mask: u8,
     pub(super) cpu_cycles: u8,
 }
+
 pub(super) const NMI: Interrupt = Interrupt {
     itype: InterruptType::NMI,
     vector_addr: 0xfffA,
     b_flag_mask: 0b00100000,
     cpu_cycles: 2,
+};
+
+pub(super) const BRK: Interrupt = Interrupt {
+    itype: InterruptType::BRK,
+    vector_addr: 0xfffe,
+    b_flag_mask: 0b0011_0000,
+    cpu_cycles: 1,
 };
