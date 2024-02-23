6beadc4c304797faf1468a4811b250b13a5a1a4f
Keinsleif <mountaindull@gmail.com>
2024-02-23 09:30:09 +0900

    [add] mem write method to square reg

--- src/apu.rs
+++ src/apu.rs
@@ -1,6 +1,6 @@
-use self::registers::SquareRegister;
-
 mod registers;
+
+use registers::SquareRegister;
 
 pub struct NesAPU {
     ch1: SquareRegister,
@@ -13,5 +13,13 @@ impl NesAPU {
             ch1: SquareRegister::new(),
             ch2: SquareRegister::new(),
         }
+    }
+
+    pub fn write_square1(&mut self, addr: u16, data: u8) {
+        self.ch1.write(addr - 0x4000, data);
+    }
+
+    pub fn write_square2(&mut self, addr: u16, data: u8) {
+        self.ch2.write(addr - 0x4004, data);
     }
 }
--- src/apu/registers/square.rs
+++ src/apu/registers/square.rs
@@ -14,4 +14,22 @@ impl SquareRegister {
             freq_high_keyon: 0x00,
         }
     }
+
+    pub fn write(&mut self, addr: u16, data: u8) {
+        match addr {
+            0x0000 => {
+                self.tone_volume = data;
+            }
+            0x0001 => {
+                self.sweep = data;
+            }
+            0x0002 => {
+                self.freq_lower = data;
+            }
+            0x0003 => {
+                self.freq_high_keyon = data;
+            }
+            _ => panic!("not possible")
+        }
+    }
 }
