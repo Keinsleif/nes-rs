46ffe7dc661aededd3e114bcb1ae06dff7b5941e
Keinsleif <mountaindull@gmail.com>
2024-02-23 22:02:27 +0900

    [update] fn return SquareNote

--- src/apu/registers/square.rs
+++ src/apu/registers/square.rs
@@ -1,3 +1,7 @@
+use crate::apu::sounds::SquareNote;
+
+const CPU_FREQ: f32 = 1789773.0;
+
 pub struct SquareRegister {
     tone_volume: u8,
     sweep: u8,
@@ -13,6 +17,36 @@ impl SquareRegister {
             freq_lower: 0x00,
             freq_high_keyon: 0x00,
         }
+    }
+
+    pub fn get_note(&self) -> SquareNote {
+        SquareNote {
+            freq: self.get_freq(),
+            volume: self.get_volume(),
+            duty: self.get_duty(),
+        }
+    }
+
+    fn get_duty(&self) -> f32 {
+        let flag = (self.tone_volume & 0b1100_0000) >> 6;
+        match flag {
+            0b00 => 0.125,
+            0b01 => 0.25,
+            0b10 => 0.50,
+            0b11 => 0.75,
+            _ => panic!("not possible")
+        }
+    }
+
+    fn get_volume(&self) -> f32 {
+        let vol = self.tone_volume & 0b0000_1111;
+        vol as f32 / 0b0000_1111 as f32
+    }
+
+    fn get_freq(&self) -> f32 {
+        let lower = self.freq_lower;
+        let higher = self.freq_high_keyon & 0b0000_0111;
+        CPU_FREQ / (16*(((higher as u16) << 8) | (lower as u16)+1)) as f32
     }
 
     pub fn write(&mut self, addr: u16, data: u8) {
