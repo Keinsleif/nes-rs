9dc87ad30a53d3efbd4cdcc5c99fd339df223901
Keinsleif <mountaindull@gmail.com>
2024-02-23 22:04:38 +0900

    [add] send function

--- src/apu.rs
+++ src/apu.rs
@@ -1,23 +1,30 @@
 mod registers;
 pub mod sounds;
 
+use std::sync::mpsc::Sender;
+
 use registers::SquareRegister;
+
+use self::sounds::SquareNote;
 
 pub struct NesAPU {
     ch1: SquareRegister,
     ch2: SquareRegister,
+    tx: Sender<SquareNote>,
 }
 
 impl NesAPU {
-    pub fn new() -> Self {
+    pub fn new(tx: Sender<SquareNote>) -> Self {
         NesAPU {
             ch1: SquareRegister::new(),
             ch2: SquareRegister::new(),
+            tx,
         }
     }
 
     pub fn write_square1(&mut self, addr: u16, data: u8) {
         self.ch1.write(addr - 0x4000, data);
+        self.tx.send(self.ch1.get_note()).unwrap();
     }
 
     pub fn write_square2(&mut self, addr: u16, data: u8) {
