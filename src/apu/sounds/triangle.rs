19551d5915f9b8249ef7e48011612a88df21653c
Keinsleif <mountaindull@gmail.com>
2024-02-26 03:46:39 +0900

    [update] uncomment unused const

--- src/apu/sounds/triangle.rs
+++ src/apu/sounds/triangle.rs
@@ -20,10 +20,10 @@ pub struct TriangleSound {
 }
 
 impl TriangleSound {
-    const OUTPUT: [u8; 32] = [
-        15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-    ];
+    // const OUTPUT: [u8; 32] = [
+    //     15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+    //     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+    // ];
     pub fn new(sample_rate: f32) -> (Self, Sender<TriangleNote>) {
         let (tx, rx) = channel::<TriangleNote>();
         (
