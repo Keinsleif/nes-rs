1d9afb370a2609daedcffb72f3f06ce4730b63a5
Keinsleif <mountaindull@gmail.com>
2024-02-26 03:46:01 +0900

    [add] triangle sound

--- src/apu/sounds.rs
+++ src/apu/sounds.rs
@@ -1,22 +1,27 @@
 mod noise;
+mod triangle;
 mod pulse;
 
 use std::sync::mpsc::Sender;
 
 pub use noise::NoiseNote;
 pub use noise::NoiseSound;
+pub use triangle::TriangleNote;
+pub use triangle::TriangleSound;
 pub use pulse::PulseNote;
 pub use pulse::PulseSound;
 
 pub struct Transmitters {
     pub square1: Sender<PulseNote>,
     pub square2: Sender<PulseNote>,
+    pub triangle: Sender<TriangleNote>,
     pub noise: Sender<NoiseNote>,
 }
 
 pub struct SoundManager {
     pub square1: PulseSound,
     pub square2: PulseSound,
+    pub triangle: TriangleSound,
     pub noise: NoiseSound,
 }
 
@@ -24,16 +29,19 @@ impl SoundManager {
     pub fn new(sample_rate: f32) -> (Self, Transmitters) {
         let (square1_sound, square1_tx) = PulseSound::new(sample_rate);
         let (square2_sound, square2_tx) = PulseSound::new(sample_rate);
+        let (triangle_sound, triangle_tx) = TriangleSound::new(sample_rate);
         let (noise_sound, noise_tx) = NoiseSound::new(sample_rate);
         (
             SoundManager {
                 square1: square1_sound,
                 square2: square2_sound,
+                triangle: triangle_sound,
                 noise: noise_sound,
             },
             Transmitters {
                 square1: square1_tx,
                 square2: square2_tx,
+                triangle: triangle_tx,
                 noise: noise_tx,
             },
         )
@@ -43,6 +51,7 @@ impl SoundManager {
         let mut sound: f32 = 0.0;
         sound += self.square1.step();
         sound += self.square2.step();
+        sound += self.triangle.step();
         sound += self.noise.step();
         sound
     }
---
+++ src/apu/sounds/triangle.rs
@@ -0,0 +1,56 @@
+use std::{sync::mpsc::{channel, Receiver, Sender}, time::Duration};
+
+pub struct TriangleNote {
+    pub freq: f32,
+}
+
+impl TriangleNote {
+    pub fn new() -> Self {
+        TriangleNote {
+            freq: 0.0,
+        }
+    }
+}
+
+pub struct TriangleSound {
+    sample_rate: f32,
+    phase: f32,
+    pub note: TriangleNote,
+    pub rx: Receiver<TriangleNote>,
+}
+
+impl TriangleSound {
+    const OUTPUT: [u8; 32] = [
+        15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+    ];
+    pub fn new(sample_rate: f32) -> (Self, Sender<TriangleNote>) {
+        let (tx, rx) = channel::<TriangleNote>();
+        (
+            TriangleSound {
+                sample_rate,
+                phase: 0.0,
+                note: TriangleNote::new(),
+                rx,
+            },
+            tx,
+        )
+    }
+
+    pub fn step(&mut self) -> f32{
+        let res = self.rx.recv_timeout(Duration::ZERO);
+        match res {
+            Ok(note) => self.note = note,
+            _ => {}
+        }
+        let output = if self.phase <= 0.25 {
+            self.phase
+        } else if self.phase >= 0.25 && self.phase <= 0.75 {
+            0.5 - self.phase
+        } else {
+            self.phase - 1.0
+        };
+        self.phase = (self.phase + self.note.freq / self.sample_rate) % 1.0;
+        output * 4.0
+    }
+}
