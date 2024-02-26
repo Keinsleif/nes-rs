007d551397a7071679cb7ab064befd588c82e12c
Keinsleif <mountaindull@gmail.com>
2024-02-26 18:14:13 +0900

    [fix] chr_rom_sizeが0の時の対応

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -46,9 +46,15 @@ impl Rom {
         let prg_rom_start = 16 + if skip_trainer {512} else {0};
         let chr_rom_start = prg_rom_start + prg_rom_size;
 
+        let chr_rom = if chr_rom_size == 0 {
+            vec![0; CHR_ROM_PAGE_SIZE]
+        } else {
+            raw[chr_rom_start..(chr_rom_start + chr_rom_size)].to_vec()
+        };
+
         Ok(Rom{
             prg_rom: raw[prg_rom_start..(prg_rom_start + prg_rom_size)].to_vec(),
-            chr_rom: raw[chr_rom_start..(chr_rom_start + chr_rom_size)].to_vec(),
+            chr_rom,
             mapper,
             screen_mirroring,
         })
